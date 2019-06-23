974384f9f22d4f0128d61a7b60813fd001111945
juancjara <juancjara@pucp.pe>
2019-06-23 11:44:00 -0500

    Add custom responsive table

---
+++ src/components/ResponsiveTable.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Table, TableRow, TableHeader } from '@aragon/ui';
+
+const Wrapper = styled.div`
+  overflow-x: auto;
+`;
+
+const StyledTable = styled(Table)`
+  min-width: ${props => props.minWidth};
+`;
+
+const ResponsiveTable = ({ headers, children, minWidth }) => {
+  return (
+    <Wrapper minWidth={minWidth}>
+      <StyledTable
+        header={
+          <TableRow>
+            {headers.map(header => (
+              <TableHeader key={header} title={header} />
+            ))}
+          </TableRow>
+        }
+      >
+        {children}
+      </StyledTable>
+    </Wrapper>
+  );
+};
+
+ResponsiveTable.propTypes = {
+  children: PropTypes.element.isRequired,
+  headers: PropTypes.array.isRequired,
+  minWidth: PropTypes.string
+};
+
+ResponsiveTable.defaultProps = {
+  minWidth: '600px'
+};
+
+export default ResponsiveTable;
