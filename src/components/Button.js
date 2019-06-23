74c7988de278c14a126500a5ddcd520bd7d9aada
juancjara <juancjara@pucp.pe>
2019-06-23 13:25:32 -0500

    Fix proptype on Button component
    Use Proptype.string instead of Proptype.element, because the button only
    receives a text.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import { LoadingRing, Button as AragonButton } from '@aragon/ui';
+import { LoadingRing, Button as AragonButton } from "@aragon/ui";
 
 const StyledButton = styled(AragonButton)`
   display: flex;
@@ -20,7 +20,7 @@ const Button = ({ loading, children, ...props }) => {
 };
 
 Button.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.string.isRequired,
   loading: PropTypes.bool
 };
 
