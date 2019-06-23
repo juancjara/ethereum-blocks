ee5652ee9bd0cf8c5bc935c99f2cbb9dc6279336
juancjara <juancjara@pucp.pe>
2019-06-23 13:37:06 -0500

    Guard useLastestBlocks hook when component unmounts

--- src/hooks/useLatestBlocks.js
+++ src/hooks/useLatestBlocks.js
@@ -1,23 +1,26 @@
-import { useState, useEffect } from 'react';
-import Q from 'promise';
+import { useState, useEffect } from "react";
+import Q from "promise";
 
-import web3 from '../web3';
+import web3 from "../web3";
 
 const useLatestBlocks = numBlocks => {
   const [fetching, setFetching] = useState(true);
   const [blocks, setBlocks] = useState([]);
 
   useEffect(() => {
+    let didCancel = false;
     const fetchLatestBlocks = async () => {
       setFetching(true);
       const blockNumber = await web3.getBlockNumber();
       const blocks = await Q.all(
         [...Array(numBlocks)].map((_, i) => web3.getBlock(blockNumber - i))
       );
+      if (didCancel) return;
       setFetching(false);
       setBlocks(blocks);
     };
     fetchLatestBlocks();
+    return () => (didCancel = true);
   }, [numBlocks]);
 
   return {
